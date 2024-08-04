e25046380d2dde04664703628726f90c086db343
EC2 Default User <ec2-user@ip-172-31-25-137.ap-northeast-1.compute.internal>
2024-08-04 09:39:47 +0000

    [Add] コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/mypage'
-  get 'users/edit'
-  get 'users/show'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
   devise_for :users
   root to: "homes#top"
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:mypage, :show, :edit, :update]
+  resources :posts, only: [:new, :index, :show]
 
   get 'homes/about' => 'homes#about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
