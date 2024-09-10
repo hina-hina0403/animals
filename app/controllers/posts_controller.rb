fb649d124479ce3c77d0b4d2a4e6d263b1061520
EC2 Default User <ec2-user@ip-172-31-25-137.ap-northeast-1.compute.internal>
2024-09-10 12:58:07 +0000

    [Add] 編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :show]
+  before_action :authenticate_user!, except: [:index]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def new
